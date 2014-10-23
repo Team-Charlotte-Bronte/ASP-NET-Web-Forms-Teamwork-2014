f207b9844a53ca95599efd1726730e869ebf37dc
drumev <milan_drumev@yahoo.com>
2014-10-23 23:40:09 +0300

    Working on book details

---
+++ Bookie/Bookie.Web/Books/Details.aspx.cs
@@ -0,0 +1,20 @@
+﻿using Bookie.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bookie.Web.Books
+{
+    public partial class Details : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var queryId = "EEEB0180-2D68-40A6-80FE-F2568745E765";
+            var book = this.Data.Books.All().Where(b => b.Id.ToString() == queryId).FirstOrDefault();
+            this.BookDetailsView.DataSource = book;
+        }
+    }
+}
