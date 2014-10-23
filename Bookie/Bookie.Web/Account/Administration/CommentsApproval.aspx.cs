7a302cc2125c05ba13888bcc60d436136f301483
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-10-23 13:30:11 +0300

    admin sub-panels added

---
+++ Bookie/Bookie.Web/Account/Administration/CommentsApproval.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bookie.Web.Account.Administration
+{
+    public partial class CommentsApproval : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Bookie/Bookie.Web/Account/Administration/UsersPanel.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Account/Administration/Administration.master"
+    AutoEventWireup="true" CodeBehind="UsersPanel.aspx.cs"
+    Inherits="Bookie.Web.Account.Administration.UsersPanel" %>
+<asp:Content ID="ContentUsers" ContentPlaceHolderID="AdminContent" runat="server">
+</asp:Content>
