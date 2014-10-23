aab6fbc814884be9d36176bd4dc84d70c52e0313
drumev <milan_drumev@yahoo.com>
2014-10-24 00:35:29 +0300

    Book details

--- Bookie/Bookie.Web/Books/Details.aspx.cs
+++ Bookie/Bookie.Web/Books/Details.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using Bookie.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,9 +12,9 @@ namespace Bookie.Web.Books
     public partial class Details : BasePage
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-            var queryId = "EEEB0180-2D68-40A6-80FE-F2568745E765";
-            var book = this.Data.Books.All().Where(b => b.Id.ToString() == queryId).FirstOrDefault();
+        { 
+            var queryId = new Guid("EEEB0180-2D68-40A6-80FE-F2568745E765");
+            var book = this.Data.Books.All().Where(x => x.Id == queryId).ToList();
             this.BookDetailsView.DataSource = book;
         }
     }
