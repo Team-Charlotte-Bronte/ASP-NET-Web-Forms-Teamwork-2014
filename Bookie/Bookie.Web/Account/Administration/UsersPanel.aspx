4fb7e333995f690a76d5e4b89b19b503265cff78
Ivaylo Hristov <ivaylo1987@gmail.com>
2014-10-24 13:59:48 +0300

    user panele

--- Bookie/Bookie.Web/Account/Administration/UsersPanel.aspx
+++ Bookie/Bookie.Web/Account/Administration/UsersPanel.aspx
@@ -2,4 +2,28 @@
     AutoEventWireup="true" CodeBehind="UsersPanel.aspx.cs"
     Inherits="Bookie.Web.Account.Administration.UsersPanel" %>
 <asp:Content ID="ContentUsers" ContentPlaceHolderID="AdminContent" runat="server">
+
+     <asp:UpdatePanel runat="server" ID="UpdateGridPanel">
+        <ContentTemplate>
+
+            <asp:GridView ID="GridViewUsers" runat="server"
+                SelectMethod="GridViewUsers_GetData"
+                UpdateMethod="GridViewUsers_UpdateItem"
+                ItemType="Bookie.Models.User"
+                AllowPaging="True"
+                PageSize="5"
+                AllowSorting="True"
+                DataKeyNames="Id"
+                AutoGenerateEditButton="true"
+                AutoGenerateColumns="false"
+                CssClass="table table-hover table-striped">
+
+                <Columns>
+                    <asp:BoundField DataField="UserName" HeaderText="UserName" SortExpression="UserName" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" SortExpression="Email" />
+                </Columns>
+            </asp:GridView>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
 </asp:Content>
--- Bookie/Bookie.Web/Account/Administration/UsersPanel.aspx.cs
+++ Bookie/Bookie.Web/Account/Administration/UsersPanel.aspx.cs
@@ -1,12 +1,43 @@
 ﻿namespace Bookie.Web.Account.Administration
 {
+    using Bookie.Web.Models;
     using System;
     using System.Linq;
 
-    public partial class UsersPanel : System.Web.UI.Page
+    public partial class UsersPanel : BasePage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
+
+        // The return type can be changed to IEnumerable, however to support
+        // paging and sorting, the following parameters must be added:
+        //     int maximumRows
+        //     int startRowIndex
+        //     out int totalRowCount
+        //     string sortByExpression
+        public IQueryable<Bookie.Models.User> GridViewUsers_GetData()
+        {
+            return this.Data.Users.All().OrderBy( u => u.Id);
+        }
+
+        // The id parameter name should match the DataKeyNames value set on the control
+        public void GridViewUsers_UpdateItem(int id)
+        {
+            Bookie.Models.User item = null;
+            // Load the item here, e.g. item = MyDataLayer.Find(id);
+            if (item == null)
+            {
+                // The item wasn't found
+                ModelState.AddModelError("", String.Format("Item with id {0} was not found", id));
+                return;
+            }
+            TryUpdateModel(item);
+            if (ModelState.IsValid)
+            {
+                // Save changes here, e.g. MyDataLayer.SaveChanges();
+
+            }
         }
     }
 }
